77e8eeb3cfa4192f0c83fe0bcafe36fd037e6000
thiagolboliveira1 <thiagolboliveira@hotmail.com>
2024-06-07 13:32:59 -0300

    atualizando

--- variaveis_e_operadores/primeiro_desafio.js
+++ variaveis_e_operadores/primeiro_desafio.js
@@ -1,6 +1,6 @@
 /*Faça um programa para calcular o c=valor de uma viagem.
 
-Você terá 3 variáveis. Sendo elas:
+Você terá 5 variáveis. Sendo elas:
 1 - Preço do etanol;
 2 - Preço da gasolina;
 3 - O tipo de combustível que está no seu carro;
@@ -10,14 +10,21 @@ Você terá 3 variáveis. Sendo elas:
 Imprima no console o valor que será gasto para realizar está viagem.*/
 
 
-
-const precoCombustivel = 6.29;
+const precoEtanol = 4.39;
+const precoGasolina = 6.29;
 const combustivelPorKm = 12;
-const distanciaViagem = 1750;
+const distanciaViagem = 100;
+const tipoCombustivel = 'Gasolina';
 
 const litrosConsumidos = distanciaViagem / combustivelPorKm;
 
-const valorGasto = litrosConsumidos * combustivelPorKm
+if (tipoCombustivel === 'Etanol') {
+    const valorGasto = litrosConsumidos * precoEtanol;
+   
+    console.log(valorGasto.toFixed(2));
+} else {
+    const valorGasto = litrosConsumidos * precoGasolina;
+    console.log(valorGasto.toFixed(2));
+}
 
 
-console.log(valorGasto.toFixed(2));
