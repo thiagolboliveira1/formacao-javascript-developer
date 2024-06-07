de9ca478d87d4e44ac788ee7b4dee082ff2a8228
thiagolboliveira1 <thiagolboliveira@hotmail.com>
2024-06-07 10:44:10 -0300

    atualizando

--- variaveis_e_operadores/primeiro_desafio.js
+++ variaveis_e_operadores/primeiro_desafio.js
@@ -1,4 +1,13 @@
+/*Faça um programa para calcular o c=valor de uma viagem.
 
+Você terá 3 variáveis. Sendo elas:
+1 - Preço do etanol;
+2 - Preço da gasolina;
+3 - O tipo de combustível que está no seu carro;
+4 - Gasto médio de combustível do carro por KM;
+5 - Distância em KM da viagem;
+
+Imprima no console o valor que será gasto para realizar está viagem.*/
 
 
 
